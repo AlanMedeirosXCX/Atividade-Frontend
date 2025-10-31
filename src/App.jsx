4f771c829c985d466bf4093f7bc3a962d87dbc6f
Alan Medeiros <medeirosalan561@gmail.com>
2025-10-30 22:17:32 -0300

    React + Vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Gatinho from "./Gatinho";
+
+function App() {
+  return (
+    <div>
+      <h1>App React + Vite</h1>
+      <Gatinho />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Gatinho.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Gatinho.css";
+
+function Gatinho() {
+  return (
+    <div className="card">
+      <img 
+        src="https://placekitten.com/300/200" 
+        alt="Um gatinho fofo"
+        className="imagem"
+      />
+      <h2>Olá! quer dizer, Meow </h2>
+      <p>MEEEEEOOW</p>
+    </div>
+  );
+}
+
+export default MeuComponente;
